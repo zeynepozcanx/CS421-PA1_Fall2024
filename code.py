47e6d8cc282a149a7ae76ab4b2bc6749576bfc1a
zeynepozcanx <zeyynepozcann@outlook.com>
2024-11-05 22:03:28 +0300

    Updating or creating file

--- code.py
+++ code.py
@@ -1,7 +1,7 @@
 # CS 421 Programming Assignment 1 - PseudoGit
 
 def greet_the_student():
-    return "Hello World"
+    return "22003454"
 
 if(__name__ == "__main__"):
     print(greet_the_student())
